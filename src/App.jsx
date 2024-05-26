95fa6015073e27a27dc68122b078df9cd19861d9
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-26 13:05:44 +0530

    restarting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignUpForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
+import Home from './components/Home';
+import Services from './components/Services';
+import NavBar from './components/NavBar';
+import './App.css';
+import Healthtips from './components/Service/Health_Tips';
+import Bookappointment from './components/Service/BookAppointment';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App = () => {
+    return (
+        <div>
+          <div className="main-container">
+          <NavBar/>
+          </div>
+            
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/signup" element={<SignUpForm/>} />
+                    <Route path="/login" element={<LoginForm/>} />
+                    <Route path="/service" element={<Services/>} />
+                    <Route path="/healthtips" element={<Healthtips/>} />
+                    <Route path="/bookappointment" element={<Bookappointment/>}/>
+                </Routes>
+            </div>
+    );
+};
+
+export default App;
